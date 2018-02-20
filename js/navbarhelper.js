93ff4e61129e0aec2b2787b007ab63687c9d3ee4
ccantey <cantey0701@gmail.com>
2018-02-20 15:02:29 -0600

    add images. fiddling with navbar hover

--- js/navbarhelper.js
+++ js/navbarhelper.js
@@ -1,39 +1,60 @@
-    $(document).ready(function () {
+var temp;
+$(document).ready(function () {
      
   $('[data-toggle="tooltip"]').tooltip()
      
   $('[data-toggle="popover"]').popover()
 
-      $('.nav-item').click(function(event){
+  $('.nav-item').hover(function(event){
+    $(".collapse").collapse('hide');
+    $('.nav-item').removeClass('active');
+    if ($(this).not('active')){
+        temp=$(this).children().attr('aria-controls');
+        console.log(temp)
+        
 
-        if ($(this).hasClass('active')){
-            
-        } else {
-            $(".collapse").collapse('hide');
-            $('.nav-item').removeClass('active');
-            $(this).addClass('active')
-        }       
-      });
+        $('#'+temp).collapse('show');
+        $(this).addClass('active')
+    } else {        
+    }       
+  });
 
-      $(document).click(function (event) {
-        var clickover = $(event.target);
-        // console.log(clickover.parents());
-        var _opened = $(".collapse").hasClass("collapse show");
-        $(".collapse").collapse('hide');
-        if (_opened === true && !clickover.hasClass("dropdown-toggle")) {
-            console.log('true')
-            $(".collapse").collapse('hide');
-        }
-      });
+  $('.collapse').hover(function(){
+    $('#'+temp).collapse('show');
+  })
 
-      $('.moreAccordion').on('shown.bs.collapse', function () {
-        var textOption = $(this).attr('data-child');
-        $(textOption).html('Fewer Links');
-      });
-      $('.moreAccordion').on('hidden.bs.collapse', function () {
-        var textOption = $(this).attr('data-child');
-        $(textOption).html('More Links');
-      });
+ //  $('.nav-item a').hover(function(){
+ //    // console.log($(this).attr('aria-controls'))
+ //    temp=$(this).attr('aria-controls');
+ //    console.log(temp)
+ //    $('#'+temp).collapse('show');
+ // }, function() {
+ //    console.log(temp)
+ //    $('#'+temp).collapse('hide');
+ // });
+
+  // $(document).click(function (event) {
+  //   var clickover = $(event.target);
+  //   // console.log(clickover.parents());
+  //   var _opened = $(".collapse").hasClass("collapse show");
+  //   $(".collapse").collapse('hide');
+  //   if (_opened === true && !clickover.hasClass("dropdown-toggle")) {
+  //       console.log('true')
+  //       $(".collapse").collapse('hide');
+  //   }
+  // });
+
+
+
+
+  $('.moreAccordion').on('shown.bs.collapse', function () {
+    var textOption = $(this).attr('data-child');
+    $(textOption).html('Fewer Links');
+  });
+  $('.moreAccordion').on('hidden.bs.collapse', function () {
+    var textOption = $(this).attr('data-child');
+    $(textOption).html('More Links');
+  });
 
           
 
