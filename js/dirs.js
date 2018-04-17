feff23438951314daf6feef661bb6d5a7d6a71ce
Chris Cantey <ccantey@gis.leg.mn>
2018-04-17 09:35:00 -0500

    new image - change path (again) on dirs.js

--- js/dirs.js
+++ js/dirs.js
@@ -1,14 +1,14 @@
-const testFolder = '../img/banners/';
+const testFolder = 'img/banners/';
 const fs = require('fs');
 
 fs.unlink('banners.txt', (err) => {
-	  if (err) throw err;
-	  console.log('successfully deleted banners.txt');
-	});
+	if (err) throw err;
+	console.log('successfully deleted banners.txt');
+});
 
 fs.readdir(testFolder, (err, files) => {
   files.forEach(file => {
-    // console.log(file);
+    console.log(file);
     fs.appendFile('banners.txt', file+'\n', function (err) {
 	  if (err) {
 	    // append failed
